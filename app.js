6248ded6f2e48ba184f09cb5e882dcb5166c4180
hasankarim18 <hallysust@gmail.com>
2023-07-17 14:06:10 +0600

    get and post usgin named function and using function imported form different module

--- app.js
+++ app.js
@@ -14,18 +14,53 @@ app.get('/', (req, res)=> {
     res.send('School Database')
 } )
 
+// using name function rather than call back 
+
+const getStudents = (req, res) => {
+  db.getStudentsList()
+    .then((students) => {
+      res.status(200).send({ message: "success", data: students });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(400).send({ message: "failed", data: { err } });
+    });
+};
+
+const addnewStudents = (req, res)=> {
+    const newStudent = req.body
+  
+    db.getStudentsList()
+    .then(students => {
+         
+          const lastStudent = students[students.length - 1];
+         
+          const lastStudentId = lastStudent.id;
+          
+          const newStudentId = parseInt(lastStudentId) + 1;
+          newStudent.id = newStudentId; 
+        //   console.log(students)
+        //   console.log(newStudent)
+          students.push(newStudent)      
+
+          db.writeStudentData(students)
+          .then(()=> {
+             res.status(200).send({ message: "success", data: newStudent });
+          })
+          .catch(error => {
+             console.log(error)
+             res.status(400).send({ message: "failed", data:  error });
+          })
+    })
+    .catch((err)=> {
+         res.status(400).send({ message: "failed", data: { err } });
+    })
+}
 
 
-app.get('/students', (req, res)=> {
-   db.getStudentsList()
-     .then((students) => {
-       res.status(200).send({ message: "success", data:  students  });
-     })
-     .catch((err) => {
-        console.log(err)
-       res.status(400).send({ message: "failed", data: { err } });
-     });
-} )
+app.get('/students', getStudents)
+
+app.post('/students', addnewStudents)
 
 
 app.listen(port, ()=> {
--- db.js
+++ db.js
@@ -4,17 +4,30 @@ const fs = require('fs')
 const getStudentsList = ()=> {
     return new Promise((resolve, reject)=> {
          fs.readFile('./database/student.json','utf-8', (err, data)=> {
-            if(err){
-                console.log(err)
+            if(err){              
                 reject(err)
-            }else {
-                console.log(data)
+            }else {               
                 resolve(JSON.parse(data))
             }
         })
     })
 }
 
+const writeStudentData = (students)=> {  
+    const studentsString = JSON.stringify(students)
+    return new Promise((resolve, reject)=> {
+        fs.writeFile("./database/student.json", studentsString, (err) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve({ message: "success" });
+          }
+        });
+    })
+}
+
 
 
 module.exports.getStudentsList = getStudentsList;
+
+module.exports.writeStudentData = writeStudentData;
